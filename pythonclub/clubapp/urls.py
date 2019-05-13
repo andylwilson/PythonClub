49ebfae80e59289f3eb6142ea3f4a9b4c25a8685
Andy <38418756+andylwilson@users.noreply.github.com>
2019-05-13 03:57:19 -0700

    Assignment 6 - Meetings & Detail

--- pythonclub/clubapp/urls.py
+++ pythonclub/clubapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 # project url directs to this, which directs to the proper view
 urlpatterns=[
     path('', views.index, name='index'),
-    path('getResources/', views.getResources, name='resources')
+    path('getResources/', views.getResources, name='resources'),
+    path('getMeetings/', views.getMeetings, name='meetings'),
+    path('meetingDetail/<int:id>', views.meetingDetail, name='meetingdetail'),
 
 ]
--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, Minutes, Resource, Event
 
 # Create your views here.
@@ -9,3 +9,12 @@ def getResources(request):
     resource_list=Resource.objects.all()
     context={'resource_list' : resource_list}
     return render(request, 'clubapp/resources.html', context=context)
+
+def getMeetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request,'clubapp/meetings.html',{'meeting_list' : meeting_list})
+
+def meetingDetail(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    context={'meet' : meet,}
+    return render(request, 'clubapp/meetingdetail.html', context=context)
